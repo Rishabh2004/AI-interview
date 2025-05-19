a9c5f83091aeba3609cbdc8e4bb350b8627aa98e
Rishabh Panchal <panchalrishabh04@gmail.com>
2025-05-19 14:32:13 +0530

    Refactor code structure for improved readability and maintainability

--- app/core/config.py
+++ app/core/config.py
@@ -5,23 +5,21 @@ from pydantic_settings import BaseSettings
 class Settings(BaseSettings):
     # API details
     PROJECT_NAME: str = "AI Interview Backend"
-    DEBUG: bool = True
-    VERSION: str = "0.1.0"
 
     # Database settings
-    MONGODB_URL: str
+    DB_URL: str
     DB_NAME: str = "interview_db"
 
     # Security settings
     SECRET_KEY: str
     ALGORITHM: str = "HS256"
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
-    
+
     # Google Gemini API settings
     GEMINI_API_KEY: str
-    
+    GOOGLE_CLIENT_SECRET: str
+    GOOGLE_CLIENT_ID: str
     # CORS settings
-    ALLOWED_ORIGINS: list = ["http://localhost:3000"]
 
     class Config:
         env_file = ".env"
